4b09088c559320f5949e548ba5dbe0c2894a8e2c
Yash Jagtap <jagtapyash2512@gmail.com>
2022-12-12 15:29:37 +0530

    enum and structs declaration

--- web3/contracts/AvaxGods.sol
+++ web3/contracts/AvaxGods.sol
@@ -21,4 +21,37 @@ contract AVAXGods is ERC1155, Ownable, ERC1155Supply {
     uint256 public constant CELESTION = 5;
 
     uint256 public constant MAX_ATTACK_DEFEND_STRENGTH = 10;
+
+    enum BattleStatus {
+        PENDING,
+        STARTED,
+        ENDED
+    }
+
+    /// @dev GameToken struct to store player token information
+    struct GameToken {
+        string name; /// @param name battle card name; set by player
+        uint256 id; /// @param id battle card token id; will be randomly generated
+        uint256 attackStrength; /// @param attackStrength battle card attack; generated randomly
+        uint256 defenseStrength; /// @param defenseStrength battle card defense; generated randomly
+    }
+
+    /// @dev Player struct to store player info
+    struct Player {
+        address playerAddress; /// @param playerAddress player wallet address
+        string playerName; /// @param playerName player name; set by player during registration
+        uint256 playerMana; /// @param playerMana player mana; affected by battle results
+        uint256 playerHealth; /// @param playerHealth player health; affected by battle results
+        bool inBattle; /// @param inBattle boolean to indicate if a player is in battle
+    }
+
+    /// @dev Battle struct to store battle info
+    struct Battle {
+        BattleStatus battleStatus; /// @param battleStatus enum to indicate battle status
+        bytes32 battleHash; /// @param battleHash a hash of the battle name
+        string name; /// @param name battle name; set by player who creates battle
+        address[2] players; /// @param players address array representing players in this battle
+        uint8[2] moves; /// @param moves uint array representing players' move
+        address winner; /// @param winner winner address
+    }
 }
