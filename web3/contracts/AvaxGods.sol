7c14ec353c6356b827cbeeaacbdfef53f37a56d7
Yash Jagtap <jagtapyash2512@gmail.com>
2022-12-18 23:58:26 +0530

    function isBattle

--- web3/contracts/AvaxGods.sol
+++ web3/contracts/AvaxGods.sol
@@ -100,4 +100,13 @@ contract AVAXGods is ERC1155, Ownable, ERC1155Supply {
     function getAllPlayerTokens() public view returns (GameToken[] memory) {
         return gameTokens;
     }
+
+    // Battle getter function
+    function isBattle(string memory _name) public view returns (bool) {
+        if (battleInfo[_name] == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
