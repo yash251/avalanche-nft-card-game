9277e823ad8475fa21e59f0f306d166569f32f41
Yash Jagtap <jagtapyash2512@gmail.com>
2022-12-30 18:17:28 +0530

    title and desc

--- client/src/components/PageHOC.jsx
+++ client/src/components/PageHOC.jsx
@@ -9,6 +9,15 @@ const PageHOC = (Component, title, description) => () => {
     <div className={styles.hocContainer}>
       <div className={styles.hocContentBox}>
         <img src={logo} alt="logo" className={styles.hocLogo} onClick={() => navigate('/')} />
+
+        <div className={styles.hocBodyWrapper}>
+          <div className="flex flex-row w-full">
+            <h1 className={`flex ${styles.headText} head-text`}>{title}</h1>
+          </div>
+
+          <p className={`${styles.normalText} my-10`}>{description}</p>
+          <Component />
+        </div>
       </div>
     </div>
   );
--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -13,4 +13,11 @@ const Home = () => {
 
 export default PageHOC(
   Home,
+  <>
+    Welcome to Avax Gods <br /> a Web3 NFT Card Game
+  </>,
+  <>
+    Connect your wallet to start playing <br /> the ultimate Web3 Battle Card
+    Game
+  </>,
 );
