d69c872b0a626278a930b5a4cf7bdd6c00365d3d
Yash Jagtap <jagtapyash2512@gmail.com>
2023-01-01 16:52:26 +0530

    context blpt

---
+++ client/src/context/index.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect, useRef, useContext, createContext } from "react";
+import { ethers } from "ethers";
+import Web3Modal from "web3modal";
+import { useNavigate } from 'react-router-dom';
+
+const GlobalContext = createContext();
+
+export const GlobalContextProvider = ({ children }) => { 
+    return (
+        <GlobalContext.Provider value={{
+            demo: 'test'
+        }}>
+
+        </GlobalContext.Provider>
+    )
+}
+
+export const useGlobalContext = () => { useContext(GlobalContext) };
