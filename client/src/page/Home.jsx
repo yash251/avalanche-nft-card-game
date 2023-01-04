cb84df5aeeb531d24f2d5d19602581c80493e7ec
Yash Jagtap <jagtapyash2512@gmail.com>
2023-01-04 23:07:55 +0530

    useClobalContext

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { PageHOC } from '../components';
+import { useGlobalContext } from '../context';
 
 const Home = () => {
   return (
     <div>
-      
+      <h1 className='text-xl text-white'></h1>
     </div>
   )
 };
