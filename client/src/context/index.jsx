a44a329d4341651ea4cf93dc14a45f88296334a0
Yash Jagtap <jagtapyash2512@gmail.com>
2023-01-15 17:04:49 +0530

    setProvider

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -11,7 +11,7 @@ export const GlobalContextProvider = ({ children }) => {
     const [walletAddress, setWalletAddress] = useState('');
     const [provider, setProvider] = useState('');
     const [contract, setContract] = useState('');
-    
+
     const updateCurrentWalletAddress = async () => {
         const accounts = await window.ethereum.request({
             method: 'eth_requestAccounts'
@@ -29,6 +29,8 @@ export const GlobalContextProvider = ({ children }) => {
             const newProvider = new ethers.providers.Web3Provider(connection);
             const signer = newProvider.signer();
             const newContract = new ethers.Contract(ADDRESS, ABI, signer);
+
+            setProvider(newProvider);
         }
         setSmartContractAndProvider();
     }, []);
