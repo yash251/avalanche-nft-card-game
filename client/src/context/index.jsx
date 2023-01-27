5688fc30b4494dfe4db89c0ac02b844f34d3c43a
Yash Jagtap <jagtapyash2512@gmail.com>
2023-01-27 19:18:38 +0530

    error

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -11,6 +11,7 @@ export const GlobalContextProvider = ({ children }) => {
     const [walletAddress, setWalletAddress] = useState('');
     const [provider, setProvider] = useState('');
     const [contract, setContract] = useState('');
+    const [showAlert, setShowAlert] = useState({ status: false, type: 'info', message: '' });
 
     // set the wallet address to the state
     const updateCurrentWalletAddress = async () => {
@@ -43,9 +44,20 @@ export const GlobalContextProvider = ({ children }) => {
         setSmartContractAndProvider();
     }, []);
 
+    useEffect(() => {
+        if(showAlert?.status) {
+            const timer = setTimeout(() => {
+                setShowAlert({ status: false, type: 'info', message: '' });
+            }, 5000);
+
+            return () => clearTimeout(timer);
+        }
+    }, [showAlert]);
+    
+
     return (
         <GlobalContext.Provider value={{
-            contract, walletAddress
+            contract, walletAddress, showAlert, setShowAlert
         }}>
             {children}
         </GlobalContext.Provider>
