7bd8d1a1939ab4f1361465bf5311d51ca8f55c28
Yash Jagtap <jagtapyash2512@gmail.com>
2023-02-02 21:14:49 +0530

    Alert complete

--- client/src/components/Alert.jsx
+++ client/src/components/Alert.jsx
@@ -3,8 +3,14 @@ import React from "react";
 import { AlertIcon } from "../assets";
 import styles from "../styles";
 
-const Alert = () => {
-  return <div>Alert</div>;
+const Alert = ({ type, message }) => {
+    return (
+        <div className={`${styles.alertContainer} ${styles.flexCenter}`}>
+            <div className={`${styles.alertWrapper} ${styles[type]}`}>
+                <AlertIcon type={type}/> {message}
+            </div>
+        </div>
+    );
 };
 
 export default Alert;
