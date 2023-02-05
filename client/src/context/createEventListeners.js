a9f5f704042433b5295a3b72e04557471a64a8e1
Yash Jagtap <jagtapyash2512@gmail.com>
2023-02-05 21:49:04 +0530

    addNewEvent

--- client/src/context/createEventListeners.js
+++ client/src/context/createEventListeners.js
@@ -3,8 +3,14 @@ import React from "react";
 import { ethers } from "ethers";
 import { ABI } from "../contract";
 
-const createEventListeners = () => {
-  return <div>createEventListeners</div>;
+const addNewEvent = (eventFilter, provider, cb) => {
+    provider.removeListener(eventFilter);
+
+    provider.on(eventFilter, (Logs) => {
+        const parsedLog = (new ethers.utils.Interface(ABI)).parseLog(Logs);
+
+        cb(parsedLog);
+    })
 };
 
 export default createEventListeners;
