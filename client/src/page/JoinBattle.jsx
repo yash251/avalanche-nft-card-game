369fe7f6c1c73106251b760bb8aeed74e30f231f
Yash Jagtap <jagtapyash2512@gmail.com>
2023-03-01 23:57:14 +0530

    extra space

--- client/src/page/JoinBattle.jsx
+++ client/src/page/JoinBattle.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-
 import { useGlobalContext } from "../context";
 import { CustomButton, PageHOC } from "../components";
 import styles from "../styles";
