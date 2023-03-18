c5e3257b4bd2eb7cdf8f68466ece54cd6e442f1f
Yash Jagtap <jagtapyash2512@gmail.com>
2023-03-18 23:31:20 +0530

    getPlayerInfo complete

--- client/src/page/Battle.jsx
+++ client/src/page/Battle.jsx
@@ -33,9 +33,20 @@ const Battle = () => {
                 const p1TokenData = await contract.getPlayerToken(player01Address);
                 const player01 = await contract.getPlayer(player01Address);
                 const player02 = await contract.getPlayer(player02Address);
+
+                const p1Att = p1TokenData.attackStrength.toNumber();
+                const p1Def = p1TokenData.defenseStrength.toNumber();
+
+                const p1H = player01.playerHealth.toNumber();
+                const p1M = player01.playerMana.toNumber();
+                const p2H = player02.playerHealth.toNumber();
+                const p2M = player02.playerMana.toNumber();
+
+                setPlayer1({ ...player01, att: p1Att, def: p1Def, health: p1H, mana: p1M });
+                setPlayer2({ ...player02, att: "X", def: "X", health: p2H, mana: p2M });
             }
             catch (error) {
-                
+                console.log(error);
             }
         }
     
