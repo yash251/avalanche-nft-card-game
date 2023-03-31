c73f58f59d2d6a16a309f4b03a2bb5589f0963ce
Yash Jagtap <jagtapyash2512@gmail.com>
2023-03-31 19:29:17 +0530

    import newly created components

--- client/src/page/Battle.jsx
+++ client/src/page/Battle.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 
 import styles from "../styles";
-import { Alert } from "../components";
+import { ActionButton, Card, GameInfo, PlayerInfo, Alert } from "../components";
 import { useGlobalContext } from "../context";
 import { attack, attackSound, defense, defenseSound, player01 as player01Icon, player02 as player02Icon } from "../assets";
 import { playAudio } from '../utils/animation.js';
