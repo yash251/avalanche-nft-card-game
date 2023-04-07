118252e232a7ecd8c82ca620f1339dde831341ea
Yash Jagtap <jagtapyash2512@gmail.com>
2023-04-07 22:06:19 +0530

    playerInfo player

--- client/src/components/PlayerInfo.jsx
+++ client/src/components/PlayerInfo.jsx
@@ -1,8 +1,14 @@
 import ReactTooltip from "react-tooltip";
 import styles from "../styles";
 
-const PlayerInfo = () => {
-  return <div>PlayerInfo</div>;
+const healthPoints = 25;
+
+const PlayerInfo = ({ player, playerIcon, mt }) => {
+  return (
+    <div className={`${styles.flexCenter} ${mt ? 'mt-4' : 'mb-4'}`}>
+      <img data-for={`Player-${mt ? '1' : '2'}`} data-tip src={playerIcon} alt="player2" className="w-14 h-14 object-contain rounded-full" />
+    </div>
+  )
 };
 
 export default PlayerInfo;
