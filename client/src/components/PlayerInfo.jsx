dd7a7c703962c97e12e35584286476e38ca1111d
Yash Jagtap <jagtapyash2512@gmail.com>
2023-04-08 18:37:18 +0530

    playerInfo Health

--- client/src/components/PlayerInfo.jsx
+++ client/src/components/PlayerInfo.jsx
@@ -7,6 +7,13 @@ const PlayerInfo = ({ player, playerIcon, mt }) => {
   return (
     <div className={`${styles.flexCenter} ${mt ? 'mt-4' : 'mb-4'}`}>
       <img data-for={`Player-${mt ? '1' : '2'}`} data-tip src={playerIcon} alt="player2" className="w-14 h-14 object-contain rounded-full" />
+
+      <div className={styles.playerHealth}
+        data-for={`Health-${mt ? '1' : '2'}`}
+        data-tip={`Health: ${player?.health}`}
+      >
+        
+      </div>
     </div>
   )
 };
