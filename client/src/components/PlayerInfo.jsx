655cb0e6307a3180c46839ea4c3c12bcbf43516e
Yash Jagtap <jagtapyash2512@gmail.com>
2023-04-11 23:27:54 +0530

    marginIndexing

--- client/src/components/PlayerInfo.jsx
+++ client/src/components/PlayerInfo.jsx
@@ -3,7 +3,9 @@ import styles from "../styles";
 
 const healthPoints = 25;
 
-const healthLevel = (points) => (points >= 12 ? 'bg-green-500' : points >=6 ? 'bg-orange-500' : 'bg-red-500')
+const healthLevel = (points) => (points >= 12 ? 'bg-green-500' : points >= 6 ? 'bg-orange-500' : 'bg-red-500')
+
+const marginIndexing = (index) => index !== healthPoints - 1 ? 'mr-1' : 'mr-0'
 
 const PlayerInfo = ({ player, playerIcon, mt }) => {
   return (
@@ -17,7 +19,7 @@ const PlayerInfo = ({ player, playerIcon, mt }) => {
         {[...Array(player.health).keys()].map((item, index) => (
           <div>
             key={`player-item-${item}`}
-            className={`${styles.playerHealthBar} ${healthLevel(player.health)}`}
+            className={`${styles.playerHealthBar} ${healthLevel(player.health)} ${marginIndexing(index)}`}
           </div>
         ))}
       </div>
