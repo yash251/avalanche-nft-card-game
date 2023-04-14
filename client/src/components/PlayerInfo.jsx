860331d44c6cf404e147d3f10de501185c62e829
Yash Jagtap <jagtapyash2512@gmail.com>
2023-04-14 22:41:39 +0530

    health mana tooltip

--- client/src/components/PlayerInfo.jsx
+++ client/src/components/PlayerInfo.jsx
@@ -41,6 +41,9 @@ const PlayerInfo = ({ player, playerIcon, mt }) => {
           <span className={`${styles.playerInfoSpan}`}>Address: </span> {player?.playerAddress?.slice(0, 10)}
         </p>
       </ReactTooltip>
+
+      <ReactTooltip id={`Health-${mt ? '1' : '2'}`} effect="solid" backgroundColor="#7f46f0" />
+      <ReactTooltip id={`Mana-${mt ? '1' : '2'}`} effect="solid" backgroundColor="#7f46f0" />
     </div>
   )
 };
