8050cca7ac04869a71868251bb2747087f1b78f9
Yash Jagtap <jagtapyash2512@gmail.com>
2023-04-15 23:13:57 +0530

    Card imports

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import Tilt from 'react-parallax-tilt';
+
+import styles from "../styles";
+import { allCards } from '../assets';
 
 const Card = () => {
   return <div>Card</div>;
