3337750af7bce4f9453921e4a1068722578937b9
Yash Jagtap <jagtapyash2512@gmail.com>
2023-04-16 23:38:51 +0530

    generateRandomCardImage

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -4,7 +4,12 @@ import Tilt from 'react-parallax-tilt';
 import styles from "../styles";
 import { allCards } from '../assets';
 
-const Card = () => {
+const generateRandomCardImage = () => allCards[Math.floor(Math.random() * allCards.length - 1)];
+
+const img1 = generateRandomCardImage();
+const img2 = generateRandomCardImage();
+
+const Card = ({ card, title, restStyles, cardRef, playerTwo }) => {
   return <div>Card</div>;
 };
 
