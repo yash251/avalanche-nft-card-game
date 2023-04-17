91be29c77961ccc0c922210d1da9e10c9019aafe
Yash Jagtap <jagtapyash2512@gmail.com>
2023-04-17 22:59:43 +0530

    card img blpt

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -4,13 +4,17 @@ import Tilt from 'react-parallax-tilt';
 import styles from "../styles";
 import { allCards } from '../assets';
 
-const generateRandomCardImage = () => allCards[Math.floor(Math.random() * allCards.length - 1)];
+const generateRandomCardImage = () => allCards[Math.floor(Math.random() * (allCards.length - 1))];
 
 const img1 = generateRandomCardImage();
 const img2 = generateRandomCardImage();
 
 const Card = ({ card, title, restStyles, cardRef, playerTwo }) => {
-  return <div>Card</div>;
+  return (
+    <div className={`${styles.cardContainer} ${restStyles}`}>
+      <img src={playerTwo ? img1 : img2} alt="card" className={styles.cardImg} />
+    </div>
+  );
 };
 
 export default Card;
