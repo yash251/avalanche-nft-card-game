053e1c633e2f12c6db906da2f0647982fc1a00c2
Yash Jagtap <jagtapyash2512@gmail.com>
2023-04-18 23:44:09 +0530

    cardPointContainer

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -13,6 +13,18 @@ const Card = ({ card, title, restStyles, cardRef, playerTwo }) => {
   return (
     <div className={`${styles.cardContainer} ${restStyles}`}>
       <img src={playerTwo ? img1 : img2} alt="card" className={styles.cardImg} />
+
+      <div className={`${styles.cardPointContainer} sm:left-[21.2%] left-[22%] ${styles.flexCenter}`}>
+        <p className={`${styles.cardPoint} text-yellow-400`}>
+          {card.att}
+        </p>
+      </div>
+
+      <div className={`${styles.cardPointContainer} sm:left-[14.2%] left-[15%] ${styles.flexCenter}`}>
+        <p className={`${styles.cardPoint} text-red-700`}>
+          {card.att}
+        </p>
+      </div>
     </div>
   );
 };
