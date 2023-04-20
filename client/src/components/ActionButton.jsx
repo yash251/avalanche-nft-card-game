b6ac46892df8d23452bb6bcf149e83702a5d0fa2
Yash Jagtap <jagtapyash2512@gmail.com>
2023-04-20 23:26:39 +0530

    actionbuton blpt

--- client/src/components/ActionButton.jsx
+++ client/src/components/ActionButton.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-const ActionButton = () => {
-  return <div>ActionButton</div>;
+import styles from "../styles";
+
+const ActionButton = ({ imgUrl, handleClick, restStyles }) => {
+  return (
+    <div className={`${styles.gameMoveBox} ${styles.flexCenter} ${styles.glassEffect} ${restStyles}`}>
+      <img src={imgUrl} alt="action_img" className={styles.gameMoveIcon} />
+    </div>
+  );
 };
 
 export default ActionButton;
