e5fdc5ddaf7796925ce6bc3b28b7669e2de23482
Yash Jagtap <jagtapyash2512@gmail.com>
2023-05-02 23:40:53 +0530

    makeAMove()

--- client/src/page/Battle.jsx
+++ client/src/page/Battle.jsx
@@ -73,7 +73,7 @@ const Battle = () => {
                 <div className='flex items-center flex-row'>
                     <ActionButton
                         imgUrl={attack}
-                        handleClick={() => { }}
+                        handleClick={() => makeAMove(1)}
                         restStyles='mr-2 hover:border-yellow-400'
                     />
 
@@ -86,7 +86,7 @@ const Battle = () => {
 
                     <ActionButton
                         imgUrl={defense}
-                        handleClick={() => { }}
+                        handleClick={() => makeAMove(1)}
                         restStyles='ml-6 hover:border-red-600'
                     />
                 </div>
